443aa629ebda3fd56c114cf808600f974a041008
EliseJulio <j.hakiziman1@alustudent.com>
2025-07-26 23:21:59 +0200

    Modifs

--- frontend/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from 'react';
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-);
